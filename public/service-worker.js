3860b3145365f9d7345039a7861ada2c57b51702
Yna Mataya <ylmataya@yahoo.com>
2020-05-19 18:06:03 -0500

    cache not working

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,57 +1,69 @@
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.html",
+  "/style.css",
+  "/manifest.webmanifest",
+  "/index.js",
+  "/icons/icon_192x192.png",
+  "/icons/icon_512x512.png"
+];
+  
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/index.html",
-    "/style.css",
-    "/manifest.webmanifest",
-    "/index.js",
-    "/icons/icon_192x192.png",
-    "/icons/icon_512x512.png"
-  ];
-  
-  self.addEventListener("install", event => {
-    event.waitUntil(
-      caches.open(CACHE_NAME)
-        .then(cache => {
-          console.log("cache open")
-          return cache.addAll(FILES_TO_CACHE)
-        })
-    );
-  });
-  
-  self.addEventListener("fetch", event => {
-    if (event.request.url.includes("/api/")) {
-      event.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cachedResponse => {
-          return fetch(event.request)
-          .then(response => {
-            console.log(response)
-            if (response.status === 200) {
-              cache.put(event.request.url, response.clone())
-            } 
-            return response
-          }) .catch(err => {
-            return cache.match(event.request)
-          }) 
-        })
-        .catch(err => {
-          console.log(err)
+// install
+self.addEventListener("install", function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
         })
       );
-      return;
-    }
-    event.respondWith(
-      fetch(event.request).catch(function() {
-        return caches.match(event.request).then(function(response) {
-          if (response) {
+    })
+  );
+
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
             return response;
-          } else if (event.request.headers.get("accept").includes("text/html")) {
-            return caches.match("/");
-          }
-        });
-      })
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
     );
-  });
+
+    return;
+}});
+  
+  
